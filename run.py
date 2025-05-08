6308f37b32d7521c398f9efe9751ef0fe763942b
Krzysztof Kleszcz <krzysztofkleszcz50@gmail.com>
2025-05-08 18:50:09 +0200

    Update54656 run.py

--- run.py
+++ run.py
@@ -1,2 +1,4 @@
-import stre
-pg.run(
+import streamlit as st 
+pages = { "Data4Play 💻": [ st.Page("main_page/main_page.py", title="Let's start here - Welcome! 🙌"), st.Page("titanic/titanic.py", title="LearningApp - Titanic! 🚢"), st.Page("company/company.py", title="LearningApp - Company! 🏢"), st.Page("survey/finding_friends.py", title="LookingApp - Friends! 🕵️‍♀️"), st.Page("marathon/marathon.py", title="SwitchingApp - Marathon! 🏃‍♂️") ], } 
+pg = st.navigation(pages) 
+pg.run()
