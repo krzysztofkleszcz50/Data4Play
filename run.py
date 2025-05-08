d73b1bda04c2704abd5c4e01359472a1caac2705
Krzysztof Kleszcz <krzysztofkleszcz50@gmail.com>
2025-05-08 18:05:51 +0200

    Update 6run.py

--- run.py
+++ run.py
@@ -1,32 +1,4 @@
-import streamlit as st
-
-# Definicja stron jako funkcji
-def main_page():
-    st.write("Let's start here - Welcome! 🙌": "main_page/main_page.py")
-
-def titanic():
-    st.title("LearningApp - Titanic! 🚢")
-
-def company():
-    st.title("LearningApp - Company! 🏢")
-
-def finding_friends():
-    st.title("LookingApp - Friends! 🕵️‍♀️")
-
-def marathon():
-    st.title("SwitchingApp - Marathon! 🏃‍♂️")
-
-# Słownik stron
-pages = {
-    "Let's start here - Welcome! 🙌": main page
-    "LearningApp - Titanic! 🚢": titanic
-    "LearningApp - Company! 🏢": company,
-    "LookingApp - Friends! 🕵️‍♀️": finding_friends,
-    "SwitchingApp - Marathon! 🏃‍♂️": marathon,
-}
-
-# Nawigacja w sidebarze
-selected_page = st.sidebar.selectbox("Wybierz stronę", list(pages.keys()))
-
-# Wywołanie odpowiedniej funkcji
-pages[selected_page]()
+import streamlit as st 
+pages = { "Data4Play 💻": [ st.Page("main_page/main_page.py", title="Let's start here - Welcome! 🙌"), st.Page("titanic/titanic.py", title="LearningApp - Titanic! 🚢"), st.Page("company/company.py", title="LearningApp - Company! 🏢"), st.Page("survey/finding_friends.py", title="LookingApp - Friends! 🕵️‍♀️"), st.Page("marathon/marathon.py", title="SwitchingApp - Marathon! 🏃‍♂️") ], } 
+pg = st.navigation(pages) 
+pg.run()
