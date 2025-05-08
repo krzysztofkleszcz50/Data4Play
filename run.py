fa75955d3351fbee6cdd0655aaba087982d41f53
Krzysztof Kleszcz <krzysztofkleszcz50@gmail.com>
2025-05-08 18:34:58 +0200

    Update34234 run.py

--- run.py
+++ run.py
@@ -1,23 +1,23 @@
 import streamlit as st
 
-# Importujemy moduły statycznie
-from main_page import main_page/main_page.py
-from titanic import titanic/titanic.py
-from company import company.py
+# Correct imports for the respective modules
+from main_page import main_page
+from titanic import titanic
+from company import company
 from survey import finding_friends
 from marathon import marathon
 
-# Tworzymy słownik ze stronami i ich powiązanymi funkcjami
+# Dictionary mapping pages to their respective functions
 pages = {
-    "Let's start here - Welcome! 🙌": main_page.run,
+    "Let's start here - Welcome! 🙌": main_page.py,
     "LearningApp - Titanic! 🚢": titanic.run,
     "LearningApp - Company! 🏢": company.run,
     "LookingApp - Friends! 🕵️‍♀️": finding_friends.run,
     "SwitchingApp - Marathon! 🏃‍♂️": marathon.run
 }
 
-# Tworzenie nawigacji w sidebarze
+# Sidebar navigation for selecting a page
 selected_page = st.sidebar.selectbox("Wybierz stronę", list(pages.keys()))
 
-# Wywołanie wybranej strony
+# Execute the selected page function
 pages[selected_page]()
